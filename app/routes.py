457e5d415b952777db2c7ff81d99046d1af95d97
waydegg <ztboneg@gmail.com>
2019-04-24 23:20:36 -0700

    wip template lesson

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Wayde'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'What a great day outside!'
+        },
+        {
+            'author': {'username': 'Dude'},
+            'body': 'Wheres my car?'
+        }
+    ]
+
+
+    return render_template('index.html', user=user, posts=posts)
